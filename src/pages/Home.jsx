cd894b3cfae0d285295be331bf0964d4624e34e8
Foxtrot-14 <noamanahmedai@gmail.com>
2023-11-18 00:50:17 +0530

    home-page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,20 @@
 import "./Home.css";
 import hero from "../assets/img/sample.png";
-import sec from "../assets/img/sample2.avif";
-import sec1 from "../assets/img/sample3.avif";
+import vid from "../assets/vid/video.webm";
 const Home = () => {
   return (
     <div>
-      <div className="main">
-        <h1 className="title">नज़रZONED</h1>
-        <img src={hero} className="hero" alt="hero" />
-        <h2 className="two">Be Bold, Be You, Be Nazar Zoned</h2>
-      </div>
-      <div className="sec">
-        <div className="con">
-          <img src={sec} className="ima" alt="img" />
-          <p className="para">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae
-            quam dolor quo? Veritatis temporibus dolorum, libero, excepturi
-            eligendi maiores reprehenderit sed labore iusto, ipsam incidunt
-            animi aspernatur nemo laudantium!
-          </p>
-        </div>
-        <div className="con">
-          <p className="para">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod beatae
-            quam dolor quo? Veritatis temporibus dolorum, libero, excepturi
-            eligendi maiores reprehenderit sed labore iusto, ipsam incidunt
-            animi aspernatur nemo laudantium!
-          </p>
-          <img src={sec1} className="ima1" alt="img" />
+      <div className="scene1">
+        <div className="cen">
+          <video src={vid} autoPlay loop muted className="vid"></video>
+          <h1 className="title">
+            <span className="naz">नज़र</span>
+            <span className="zon">ZONED</span>
+          </h1>
+          <a href="/shop" className="click">
+            Shop Now
+          </a>
+          <h2 className="sub">Elevate Your Wardrobe with NAZAR ZONED</h2>
         </div>
       </div>
     </div>
